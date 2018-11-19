c00ef0ef4b1bc3318382794412b26f68bfee7b3b
Autophagy <mika@crate.io>
2018-11-19 16:18:10 +0100

    Split leomu into core and projects, dynamically load all yml within leomu

--- hraew/leomu.py
+++ hraew/leomu.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import yaml
-from os.path import dirname, join
+from os.path import dirname, join, splitext
+from os import listdir
 import bleach
 import re
 from flask import url_for, render_template
@@ -38,11 +39,15 @@ class Lim(object):
 
         return self._body_html
 
+leomu = {}
 
-with open(join(dirname(__file__), "leomu/leomu.yml"), "r") as leomu_yml:
-    leomu = yaml.load(leomu_yml)
-    for key, value in leomu.items():
-        leomu[key] = Lim(key, value)
+for f in listdir(join(dirname(__file__), "leomu")):
+    _, ext = splitext(f)
+    if ext == ".yml" or ext == ".yaml":
+        with open(join(dirname(__file__), "leomu", f), "r") as leomu_yml:
+            lim = yaml.load(leomu_yml)
+            for key, value in lim.items():
+                leomu[key] = Lim(key, value)
 
 project_areas = ["RES", "DES", "DEV", "DOC", "TST"]
 
